130151cedbbdd7d6d51ef0131e962502375a67b7
Aditeya <aditeya.baral@gmail.com>
2022-10-29 21:07:18 +0530

    Bug fixes

--- app/app.py
+++ app/app.py
@@ -92,6 +92,7 @@ def get_election_data_get(request):
         candidates,
     )
 
+
 def update_election_with_new_data(new_election_data):
     election_id = new_election_data["election_id"]
     created_by = (
@@ -99,22 +100,39 @@ def update_election_with_new_data(new_election_data):
         if request.headers.getlist("X-Forwarded-For")
         else request.remote_addr
     )
-    current_election_data = election_db.get_election_data_by_id_and_creator(election_id, created_by)
+    current_election_data = election_db.get_election_data_by_id_and_creator(
+        election_id, created_by
+    )
     for key in new_election_data:
         if key in current_election_data:
             if key == "candidates":
-                if set(current_election_data["candidates"]) != set(new_election_data["candidates"]):
+                if set(current_election_data["candidates"]) != set(
+                    new_election_data["candidates"]
+                ):
                     current_election_data["votes"] = None
                     current_election_data["round_number"] = None
                     current_election_data["winner"] = None
-                    current_election_data["candidates"] = new_election_data["candidates"]
-                    logging.warning("Candidates changed. Resetting votes, round_number, winner")
-            elif key in ["election_id", "created_at", "created_by", "votes", "round_number", "winner"]:
-                logging.warning(f"Attempt to update {key} not allowed for election {election_id}")
+                    current_election_data["candidates"] = new_election_data[
+                        "candidates"
+                    ]
+                    logging.warning(
+                        "Candidates changed. Resetting votes, round_number, winner"
+                    )
+            elif key in [
+                "election_id",
+                "created_at",
+                "created_by",
+                "votes",
+                "round_number",
+                "winner",
+            ]:
+                logging.warning(
+                    f"Attempt to update {key} not allowed for election {election_id}"
+                )
                 continue
             else:
                 current_election_data[key] = new_election_data[key]
-    
+
     start_time = current_election_data["start_time"]
     try:
         start_time = datetime.datetime.strptime(start_time, "%Y-%m-%d %H:%M:%S")
@@ -499,27 +517,6 @@ def remove_vote(election_id):
         output["error"] = str(e)
         response_code = 400
     return jsonify(output), response_code
-
-@app.route("/update", methods=["POST"])
-def update_election():
-    new_election_data = request.get_json()
-    logging.debug(f"New election data: {new_election_data}")
-    output = dict()
-    if new_election_data:
-        try:
-            updated_election_data = update_election_with_new_data(new_election_data)
-            output["status"] = True
-            output["message"] = "Election updated successfully."
-            output["data"] = updated_election_data
-            response_code = 200
-        except Exception as e:
-            logging.error(f"Exception occurred while updating election: {e}")
-            output["status"] = False
-            output["message"] = "Error occurred while updating election."
-            response_code = 400
-    
-    return jsonify(output), response_code
-
 
 
 @app.route("/update", methods=["POST"])
